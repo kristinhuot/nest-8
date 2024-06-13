4742227f25dfe900e2335b58f92084ca411d17a8
Kristin Huot <kristin.huot@gmail.com>
2024-06-13 15:23:51 -0500

    added dotenv and npm install

--- database.sql
+++ database.sql
@@ -4,6 +4,11 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "email" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL, 
+    "company_name" VARCHAR NOT NULL, 
+    "industry"
+
 );
